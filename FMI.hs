06b9c25068d3efbb561d4d080f73a3c371c2be90
Matias <mcornalo@frba.utn.edu.ar>
2023-05-23 15:30:41 -0300

    Parcial casi completo, faltaria el punto 5

--- FMI.hs
+++ FMI.hs
@@ -35,3 +35,33 @@ establecerBlindaje unPais = disminuirPuestos 500 . cambiarDeuda (pbi unPais) $ u
 
 pbi :: Pais -> Float
 pbi unPais = ingresoPerCapita unPais * (poblacionPrivada unPais + poblacionPrivada unPais) 
+
+namibia :: Pais
+namibia = Pais 4140 400000 650000 ["Mineria","Ecoturismo"] 50
+
+receta :: Estrategia
+receta  = cambiarDeuda 200 . explotarRecursoNatural "Mineria" -- Composicion de funciones y aplicacion parcial. A todas le falta el parameetro "Pais"
+
+-- ghci> receta namibia
+-- Pais {ingresoPerCapita = 4140.0, poblacionPublica = 400000.0, poblacionPrivada = 650000.0, recursosNaturales = ["Ecoturismo"], deuda = 248.0}
+
+zafanlosPaises :: [Pais] -> [Pais]
+zafanlosPaises = filter (elem "Petroleo" . recursosNaturales)   -- Composicion y orden superior
+
+totalDeuda :: [Pais] -> Float
+totalDeuda = sum . map deuda -- Composicion y orden superior 
+
+-- 5 en proceso
+-- esMayor :: Pais -> [Estrategia] -> Bool
+-- esMayor unPais [] = True
+-- esMayor unPais (x:xs) = (pbi . x) unPais &&  esMayor unPais xs
+
+--6
+recursosNaturalesInfinitos :: [String]
+recursosNaturalesInfinitos = "Energia" : recursosNaturalesInfinitos
+
+paisConRiquezaExtrema :: Pais
+paisConRiquezaExtrema = Pais 100000 100000 100000 recursosNaturalesInfinitos 0
+
+-- si evaluamos puedenZafarlosPais paisConRiquezaExtrema nunca nos va a devolver un valor porque siempre estara buscando el "Petroleo" pero nunca lo va a encontrar
+-- Gracias a la lazy evaluation podemos ejecutar la funcion totalDeuda incluso cuando tiene una lista infinita
