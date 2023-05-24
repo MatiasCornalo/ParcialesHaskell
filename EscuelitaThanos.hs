642eb76cb3bbc21e1239826f85006d3a189984dd
Matias <mcornalo@frba.utn.edu.ar>
2023-05-24 16:41:34 -0300

    Agrego parcial Escuelita Thanos casi completo, faltaria el ejercicio de recursividad

--- EscuelitaThanos.hs
+++ EscuelitaThanos.hs
@@ -70,3 +70,30 @@ elTiempo unPersonaje = unPersonaje {edad = max (div (edad unPersonaje) 2) 18}
 laGemaLoca :: Gema -> Gema
 laGemaLoca unaGema unPersonaje = unaGema unPersonaje
 
+guanteleteTrucho :: Guantelete
+guanteleteTrucho = Guantelete "Goma" [elTiempo,elAlma "Usar Mjolnir",laGemaLoca (elAlma "Programacion en Haskell")]
+
+utilizar :: Personaje -> [Gema] -> Personaje
+utilizar unPersonaje gemasNuevas = foldl (flip ($)) unPersonaje gemasNuevas
+
+--gemaMasPoderosa :: Guantelete -> Personaje -> Gema
+--gemaMasPoderosa unGuantelete unPersonaje = max (energiaPerdida unPersonaje ((head . gemas) unGuantelete)) (gemaMasPoderosa (unGuantelete{gemas = drop 1 (gemas unGuantelete)}) unPersonaje) 
+
+energiaPerdida :: Personaje -> Gema -> Int
+energiaPerdida unPersonaje unaGema = abs (energia unPersonaje - (energia . unaGema) unPersonaje) 
+
+infinitasGemas :: Gema -> [Gema]
+infinitasGemas gema = gema:(infinitasGemas gema)
+
+guanteleteDeLocos :: Guantelete
+guanteleteDeLocos = Guantelete "vesconite" (infinitasGemas elTiempo)
+
+usoLasTresPrimerasGemas :: Guantelete -> Personaje -> Personaje
+usoLasTresPrimerasGemas guantelete unPersonaje = (utilizar unPersonaje . take 3 . gemas) guantelete
+
+-- gemaMasPoderosa punisher guanteleteDeLocos. Este comando es incalculable, ya que la lista es infinita e infinitamente estamos aplicando la gema del tiempo a Punisher, por lo tanto nunca nos va a devolver el valor 
+-- usoLasTresPrimerasGemas guanteleteDeLocos punisher.  Este si va a funcionar porque la ventaja de "Lazy evaluation" es que no utiliza los valores, o no calcula los valores, hasta que realmente los necesite.
+-- En este caso se ejecuta perfectamente porque solo necesita las primeras 3 gemas
+
+punisher :: Personaje
+punisher = Personaje 10 10 "Tierra" ["Disparar"]
