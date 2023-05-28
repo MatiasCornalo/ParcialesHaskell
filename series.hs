720a6d24131a1e2f7ef95901428b670a6e6d1785
Matias <mcornalo@frba.utn.edu.ar>
2023-05-28 15:28:21 -0300

    Agrego punto 2 y 1

--- series.hs
+++ series.hs
@@ -25,4 +25,39 @@ conFavoritismos :: [Actor] -> Serie -> Serie
 conFavoritismos unosActores unaSerie = agregarActoresFav unosActores . sacarLosPrimeros2Actores $ unaSerie
 
 agregarActoresFav :: [Actor] -> Serie -> Serie
-agregarActoresFav unosActores unaSerie =  
+agregarActoresFav unosActores unaSerie =  unaSerie {actores = unosActores ++ actores unaSerie}
+
+sacarLosPrimeros2Actores :: Serie -> Serie
+sacarLosPrimeros2Actores unaSerie = unaSerie {actores = drop 2  . actores $ unaSerie }
+
+timBurton = conFavoritismos [jhonnyDepp,helenaBonhamCarter]
+
+jhonnyDepp :: Actor
+jhonnyDepp = Actor "Johnny Depp" 20000000 []
+
+helenaBonhamCarter :: Actor
+helenaBonhamCarter = Actor "Helena Bonham Carter" 15000000 []
+
+gatoPardeitor :: Serie -> Serie
+gatoPardeitor unaSerie = unaSerie 
+
+estireitor :: Serie -> Serie 
+estireitor unaSerie = unaSerie {temporadas = temporadas unaSerie  *2}
+
+desespereitor :: Serie -> Serie
+desespereitor = gatoPardeitor .  agregarActoresFav [tobeyMaguire]. estireitor
+
+tobeyMaguire :: Actor
+tobeyMaguire = Actor "Tobey Maguire" 10000000 []
+
+canceleitor :: Float -> Serie -> Serie
+canceleitor ratingMin unaSerie
+    | estaEnRojo unaSerie || esMenorARating ratingMin unaSerie = cancelarSerie unaSerie
+    | otherwise = unaSerie
+
+cancelarSerie :: Serie -> Serie
+cancelarSerie unaSerie = unaSerie {estaCancelada = True}
+
+esMenorARating :: Float -> Serie -> Bool
+esMenorARating ratingMin unaSerie = ratingMin > rating unaSerie
+
