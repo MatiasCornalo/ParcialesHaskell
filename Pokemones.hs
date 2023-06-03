30d64960db9004ced0ac9c11617b687d3cad9910
Matias Cornalo <mcornalo@frba.utn.edu.ar>
2023-06-03 10:16:12 -0300

    Agrego Parcial de pokemones

--- Pokemones.hs
+++ Pokemones.hs
@@ -11,14 +11,14 @@ type Item = Investigador -> Investigador
 data Pokemon = Pokemon{
     mote :: String,
     descripcion :: String,
-    puntos :: Int,
+    puntos :: Float,
     nivelCompaniero :: Int
 }
 
-data Rango = Cielo | Galaxia | Constelacion | Estrella
+data Rango = Cielo | Galaxia | Constelacion | Estrella deriving (Eq,Show)
 
 akari :: Investigador
-akari = Investigador Constelacion "Akari" 1500 oshawott [] []
+akari = Investigador Constelacion "Akari" 1500 oshawoot [] []
 
 oshawoot :: Pokemon
 oshawoot = Pokemon "Oshawott" "una nutria que pelea con el caparazón de su pecho" 3 5 
@@ -48,7 +48,10 @@ obtenerItem :: Item -> Actividad
 obtenerItem unItem unInvestigador = unInvestigador{mochila= unItem : mochila unInvestigador}
 
 bayas :: Item
-bayas = (^2) . cambiarExp 1
+bayas = cuadradoExp . cambiarExp 1 
+
+cuadradoExp :: Investigador -> Investigador
+cuadradoExp unInvestigador = unInvestigador {experiencia = experiencia unInvestigador ^ 2}
 
 cambiarExp :: Float -> Investigador -> Investigador
 cambiarExp num unInvestigador = unInvestigador{experiencia=experiencia unInvestigador + num}
@@ -68,4 +71,39 @@ admirarPaisaje unInvestigador = perderItems 3 . cambiarExp (0.05*(experiencia un
 perderItems :: Int -> Investigador -> Investigador
 perderItems num unInvestigador = unInvestigador {mochila = drop num $ mochila unInvestigador}
 
-capturarPokemon :: 
+capturarPokemon :: Pokemon -> Actividad
+capturarPokemon unPokemon unInvestigador
+    |   puntos unPokemon > 20   = cambiarExp (puntos unPokemon) . cambiarCompaniero unPokemon $ unInvestigador
+    |   otherwise               = cambiarExp (puntos unPokemon) unInvestigador
+
+cambiarCompaniero :: Pokemon ->  Investigador -> Investigador
+cambiarCompaniero unPokemon unInvestigador = unInvestigador{companiero = unPokemon}
+
+combatirPokemon :: Pokemon -> Actividad
+combatirPokemon unPokemon unInvestigador
+    | leGana (companiero unInvestigador) unPokemon = cambiarExp (puntos unPokemon/2) unInvestigador
+    | otherwise = unInvestigador
+
+leGana :: Pokemon -> Pokemon -> Bool
+leGana pokemon1 pokemon2 = nivelCompaniero pokemon1 > nivelCompaniero pokemon2
+
+type Expedicion = [Actividad]
+
+investigadoresConpokemonesAlfa :: [Investigador] -> Expedicion -> [String]
+investigadoresConpokemonesAlfa investigadores expedicion = map nombre . filter ((==3) . length . filter (=="Alfa"). map mote . pokemonesCapturados) . investigadoresPostExpedicion expedicion $ investigadores
+
+investigadoresGalaxia :: Expedicion -> [Investigador] -> [Float]
+investigadoresGalaxia expedicion = map experiencia . filter ((==Galaxia) . rango) . investigadoresPostExpedicion expedicion 
+
+investigadoresPostExpedicion :: Expedicion -> [Investigador] -> [Investigador]
+investigadoresPostExpedicion expedicion investigadores = map (realizarExpedicion expedicion) investigadores 
+
+pokemonesDeNivel10DeInvestigadores :: Expedicion -> [Investigador] -> [Pokemon]
+pokemonesDeNivel10DeInvestigadores expedicion investigadores = map companiero . filter((>10) . nivelCompaniero . companiero) . investigadoresPostExpedicion expedicion $ investigadores
+
+realizarExpedicion :: Expedicion -> Investigador -> Investigador
+realizarExpedicion expedicion investigador = foldl (flip ($))  investigador expedicion
+
+ultimos3PokemonesParesPostExp ::  Expedicion -> [Investigador] -> [[Pokemon]]
+ultimos3PokemonesParesPostExp expedicion investigadores = map pokemonesCapturados . filter(all even . map nivelCompaniero . pokemonesCapturados) . investigadoresPostExpedicion expedicion $ investigadores
+
